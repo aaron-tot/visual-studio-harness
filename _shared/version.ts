d4d0d0fe160c2634abeb40ee4927957c9c67b5e3
Aaron Tot <aaron-tot@users.noreply.github.com>
2026-08-15 17:25:59 -0500

    chore: bump version to 0.0.7-alpha

--- _shared/version.ts
+++ _shared/version.ts
@@ -1,3 +1,3 @@
-export const VERSION = "0.0.6-alpha";
+export const VERSION = "0.0.7-alpha";
 export const VERSION_LABEL = "Pre-Release";
 export const FULL_VERSION = `${VERSION} (${VERSION_LABEL})`;
